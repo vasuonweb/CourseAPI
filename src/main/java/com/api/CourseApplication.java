6d288630dbc36e3958da177d6eee591cdd4e6ab4
sjeenor <sjeenor@NISTMACL0017s-MacBook-Pro.local>
2017-12-19 14:34:30 +0530

    course API spring boot app changed1

--- src/main/java/com/api/CourseApplication.java
+++ src/main/java/com/api/CourseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CourseApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Testing");
 		SpringApplication.run(CourseApplication.class, args);
 		
 	}
